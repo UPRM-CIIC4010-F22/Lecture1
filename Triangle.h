f3d5d0dac76197c1365fb1cba91c32c284100d1c
Bienvenido Velez <Bienvenido.Velez@upr.edu>
2022-08-31 19:13:25 -0400

    Moved Triangle function into class

--- Triangle.h
+++ Triangle.h
@@ -1,5 +1,9 @@
+#include <cmath>
+
+using namespace std;
+
 class Triangle {
-    public:
+public:
     int side1;
     int side2;
     int side3;
@@ -9,4 +13,30 @@ class Triangle {
         this->side2 = side2;
         this->side3 = side3;
     }
+
+    double getArea() {
+        double s = (this->side1 + this->side2 + this->side3) / 2.0;
+        double area = sqrt(s * (s - this->side1) * (s - this->side2) * (s - this->side3));
+        return area;
+    }
+
+    bool hasLargerArea(Triangle t2) {
+
+        return (this->getArea() > t2.getArea());
+    }
+
+    static double sumOfAreas(Triangle triangles[], int size) {
+
+        double result = 0;
+        for (int i = 0; i < size; i++) {
+            result += triangles[i].getArea();
+        }
+        return result;
+    }
+
+    Triangle scale(double scale) {
+
+        return Triangle(this->side1 * scale, this->side2 * scale, this->side3 * scale);
+    }
+
 };
